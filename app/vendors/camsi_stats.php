e35c4ce9d9be6f9bd275fa10987ddd27ed935203
Anffernee Iverson <anffernee_zhao@hotmail.com>
2012-11-10 21:06:51 +0800

    fix a fatal "time difference" bug in "fwends.com stats driver".
    fix a fatal bug that could occor time difference between servers.
    
    Signed-off-by: Anffernee Iverson <anffernee_zhao@hotmail.com>

--- app/vendors/camsi_stats.php
+++ app/vendors/camsi_stats.php
@@ -13,7 +13,7 @@ if (($argc - 1) != 1) {//if there is 1 parameter and it must mean a date like '2
 /*
  * the following line will make the whole script exit if date string format is wrong
  */
-$date = __get_remote_date($argv[1], "GMT", -0.005);
+$date = __get_remote_date($argv[1], "GMT", -0.005, "GMT", false);
 $date_l = __get_remote_date($argv[1], "GMT", -0.005, "GMT", true);
 
 /*get the abbreviation of the site*/

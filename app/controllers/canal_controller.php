e0494347062791c61de46ac38b365fba42e58519
Ray Allen <ray@ray-Lenovo-IdeaPad-S400.(none)>
2013-08-05 00:12:39 +0800

    make it record to the stats table with the posted data from cams2
    make it record to the stats table with the posted data from cams2
    
    Signed-off-by: Ray Allen <ray@ray-Lenovo-IdeaPad-S400.(none)>

--- app/controllers/canal_controller.php
+++ app/controllers/canal_controller.php
@@ -1,5 +1,6 @@
 <?php
 App::import('vendor', 'ExtraKits', array('file' => 'extrakits.inc.php'));
+App::import('vendor', 'ZMysqlConn', array('file' => 'zmysqlConn.class.php'));
 ?>
 <?php
 class CanalController extends AppController {
@@ -25,6 +26,9 @@ class CanalController extends AppController {
 		if (isset($_POST['ch'])) {
 			$n = $_POST['ch'];
 		}
+		$now = new DateTime("now", new DateTimeZone("GMT"));
+		$err = "";
+		/*log it all*/
 		switch ($n) {
 			case -1:
 			case 0:
@@ -35,9 +39,37 @@ class CanalController extends AppController {
 				$s = "illegal post";
 				break;
 		}
+		/*actually save the data into stats*/
+		switch ($n) {
+			case 0://force ch = 0 to site cams2
+				if ($ip == "66.180.199.11" || $ip == "127.0.0.1") {
+					$conn = new zmysqlConn();
+					$sql = "select a.*, b.id as 'typeid' from view_mappings a, types b where a.username = '" . $_POST['agent'] . "' and a.siteid = b.siteid and a.abbr = 'cams2'";
+					$rs = mysql_query($sql, $conn->dblink);
+					while ($r = mysql_fetch_assoc($rs)) {
+						$agid = $r['agentid'];
+						$typeid = $r['typeid'];
+						$siteid = $r['siteid'];
+						$campid = $r['campaignid'];
+						$sales = 1;
+						$trxtime = $now->format("Y-m-d H:i:s");
+						
+						$sql = "insert into stats (agentid, raws, uniques, chargebacks, signups, frauds, sales_number, typeid, siteid, campaignid, trxtime)"
+							. " values ($agid, 0, 0, 0, 0, 0, $sales, $typeid, $siteid, '$campid', '$trxtime')";
+						//$err = $sql; continue; //for debug;
+						
+						if (mysql_query($sql, $conn->dblink) === false) {
+							$err = mysql_error();
+						}
+					}
+				}
+				break;
+		}
 		$this->set(compact("s"));
 		$this->set(compact("n"));
 		$this->set(compact("ip"));
+		$this->set("now", $now->format("Y-m-d H:i:s"));
+		$this->set(compact("err"));
 	}
 }
 	
--- app/views/canal/index.ctp
+++ app/views/canal/index.ctp
@@ -1,12 +1,11 @@
 <div style="margin-bottom:10px;border-top:0;border-left:0;border:right:0;border-bottom:solid blue 1px;width:100%;color:blue;">
 <?php echo $s; ?>
 </div>
+
 <?php
 $logpath = APP . "tmp" . DS . "canals.log";
 $from = "from canal $n: ";
-$now = new DateTime("now", new DateTimeZone("GMT"));
-$stamp =  " [" . $ip . "/" 
-	.$now->format("Y-m-d H:i:s") . "GMT]\n";
+$stamp =  " [" . $ip . "/" . $now . "GMT]\n";
 if (empty($_POST)) {
 	error_log(
 		$from . "Nothing posted here" . $stamp,
@@ -18,6 +17,17 @@ if (empty($_POST)) {
 		$from . "\n" . print_r($_POST, true) . $stamp,
 		3,
 		$logpath 
+	);
+}
+
+if (!empty($err)) {
+	$time = str_replace(" ", "", $now);
+	$time = str_replace("-", "", $time);
+	$time = str_replace(":", "", $time);
+	error_log(
+		$from . "\n" . $err . $stamp,
+		3,
+		APP . "tmp" . DS . "err_" . $time . ".log"
 	);
 }
 ?>

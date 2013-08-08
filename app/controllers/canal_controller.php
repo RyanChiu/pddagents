781d1083e246ae68a276cb42ccf3daa378d007ed
Ray Allen <ray@ray-Lenovo-IdeaPad-S400.(none)>
2013-08-08 22:21:49 +0800

    check both "GET" and "POST" when deal with the push data from cams2, anyway
    check both "GET" and "POST" when deal with the push data from cams2, anyway
    
    Signed-off-by: Ray Allen <ray@ray-Lenovo-IdeaPad-S400.(none)>

--- app/controllers/canal_controller.php
+++ app/controllers/canal_controller.php
@@ -25,6 +25,8 @@ class CanalController extends AppController {
 		$ip = __getclientip();
 		if (isset($_GET['ch'])) {
 			$n = $_GET['ch'];
+		} else if (isset($_POST['ch'])) {
+			$n = $_POST['ch'];
 		}
 		$now = new DateTime("now", new DateTimeZone("GMT"));
 		$err = "";
@@ -41,10 +43,13 @@ class CanalController extends AppController {
 		}
 		/*actually save the data into stats*/
 		if (true || $ip == "66.180.199.11" || $ip == "127.0.0.1") {
-			$type = trim($_GET['type']);
+			$type = (isset($_GET['type']) ? trim($_GET['type']) : (isset($_POST['type']) ? trim($_POST['type']) : 'ill')); 
 			$type = strtolower($type);
+			$agent = (isset($_GET['agent']) ? trim($_GET['agent']) : (isset($_POST['agent']) ? trim($_POST['agent']) : '')); 
+			$unique = (isset($_GET['unique']) ? trim($_GET['unique']) : (isset($_POST['unique']) ? trim($_POST['unique']) : ''));
+			$unique = strtolower($unique);
 			$conn = new zmysqlConn();
-			$sql = "select a.*, b.id as 'typeid' from view_mappings a, types b where a.username = '" . $_GET['agent'] . "' and a.siteid = b.siteid and a.abbr = 'cams2'";
+			$sql = "select a.*, b.id as 'typeid' from view_mappings a, types b where a.username = '$agent' and a.siteid = b.siteid and a.abbr = 'cams2'";
 			$rs = mysql_query($sql, $conn->dblink);
 			while ($r = mysql_fetch_assoc($rs)) {
 				$agid = $r['agentid'];
@@ -52,11 +57,12 @@ class CanalController extends AppController {
 				$siteid = $r['siteid'];
 				$campid = $r['campaignid'];
 				$clicks = ($type == 'click' ? 1 : 0);
+				$uniques = ($unique == 'y' ? 1 : 0);
 				$sales = ($type == 'sale' ? 1 : 0);
 				$trxtime = $now->format("Y-m-d H:i:s");
 				
 				$sql = "insert into stats (agentid, raws, uniques, chargebacks, signups, frauds, sales_number, typeid, siteid, campaignid, trxtime)"
-					. " values ($agid, $clicks, 0, 0, 0, 0, $sales, $typeid, $siteid, '$campid', '$trxtime')";
+					. " values ($agid, $clicks, $uniques, 0, 0, 0, $sales, $typeid, $siteid, '$campid', '$trxtime')";
 				//$err = $sql; continue; //for debug;
 				
 				if (mysql_query($sql, $conn->dblink) === false) {
